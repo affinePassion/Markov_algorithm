c0a4f22b8609419252ee481c3354028b51212dcc
affinePassion <dali_ban26.07@mail.ru>
2024-11-14 04:53:42 +0300

    microfix

--- markov.py
+++ markov.py
@@ -4,14 +4,11 @@ class AlgorithmMarkov:
         self._word = word
         self._rules = rules
 
-    def contains(self, s : str, sub : str):
-        return s.find(sub) != -1
-
     def find_rule(self):
         for l, r, b in self._rules:
             if self.contains(self._word, l):
                 return l, r, b
-        raise ValueError("not found")
+        raise ValueError("Правила не определены")
     
     def apply_rule(self, rule):
         l = rule[0]
@@ -24,13 +21,16 @@ class AlgorithmMarkov:
     
     def run(self):
         result = []
-        flag = False                      # whether Halting rule was applied
+        flag = False                      
         try:
-            while not flag:               # Normal rule was applied
+            while not flag:               
                 result.append(self._word)
-                self._word, flag = self.apply_alg() # apply a rule
+                self._word, flag = self.apply_alg() 
             result.append(self._word)
-        except ValueError:                # No rule was applied
+        except ValueError:               
             pass
         return result
+    
+    def contains(self, s : str, sub : str):
+        return s.find(sub) != -1
     
--- rules.py
+++ rules.py
@@ -12,7 +12,16 @@ class Rules:
         match numberTask:
             case 1:
                 self._listRules = [("1@", "2", False),
-                                   ("2@,", "3", False)]
+                                   ("2@,", "3", False),
+                                   ("3@,", "4", False),
+                                   ("4@,", "5", False),
+                                   ("5@,", "6", False),
+                                   ("6@,", "7", False),
+                                   ("7@,", "8", False),
+                                   ("8@,", "9", False),
+                                   ("9@,", "@0", False),
+                                   ("@@", "1", False),
+                                   ("@,", "", True),]
             case _:
                 raise Exception("Говно вопрос")
                 
