eb5c70db65a443f819a19bb62bbe0f1a65a2d132
affinePassion <dali_ban26.07@mail.ru>
2024-11-15 01:29:36 +0300

    two algs

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+from typing import List
 from markov import AlgorithmMarkov
-
+from rules import Rules
 
 def main():
-    successor = [("aL", "La", False),
-             ("a0", "0a", False),
-             ("a" , "b" , False),
-             ("Lb", "b0", False),
-             ("0b", "L" , True ),
-             ("b" , "L" , True ),
-             (""  , "a" , False)]
-    algMark = AlgorithmMarkov(word = "L0LL", rules = successor)
+    rule1 = Rules()
+    successor = []
 
-    print(algMark.run())
+        
+    print("Алгорифмы Маркова")
+    print("1. Увеличение числа на единицу")
+    print("2. Уменьшение числа на единицу")
+    print("3. Сложение чисел")
+    print("4. Изменение слова")
+    print("5. Слияние двух слов")
+    print("6. Убрать первую букву слова")
+    print("7. Выход")
+    
+    try:
+        numTask = int(input("Введите номер алгоритма(1..7): "))
+        if numTask < 1 or numTask > 7:
+            raise ValueError("Номер алгоритма должен быть от 1 до 7")
+        if numTask == 7:
+            exit()
+        if numTask == 1 or numTask == 2:
+            algMark = AlgorithmMarkov(word="@100@", rules=rule1.rules_for_digits(numberTask=numTask))
+        print(algMark.run())
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+    except Exception as e:
+        print(f"Неожиданная ошибка: {e}")
 
 if __name__ == "__main__":
     main()
--- rules.py
+++ rules.py
@@ -1,27 +1,47 @@
-class Rules:
-    def __init__(self, listRules):
-        self._listRules = [listRules]
+from typing import List, Tuple
 
-    def add_rule(self, newRule):
+class Rules:
+    def __init__(self, listRules: List[Tuple] = None):
+        self._listRules = listRules if listRules else []
+
+    def add_rule(self, newRule: Tuple):
         self._listRules.append(newRule)
 
-    def remove_rule(self, removedRule):
+    def remove_rule(self, removedRule: Tuple):
         self._listRules.remove(removedRule)
 
-    def create_rules(self, numberTask):
+    def rules_for_digits(self, numberTask: int) -> List[Tuple]:
         match numberTask:
             case 1:
-                self._listRules = [("1@", "2", False),
-                                   ("2@,", "3", False),
-                                   ("3@,", "4", False),
-                                   ("4@,", "5", False),
-                                   ("5@,", "6", False),
-                                   ("6@,", "7", False),
-                                   ("7@,", "8", False),
-                                   ("8@,", "9", False),
-                                   ("9@,", "@0", False),
-                                   ("@@", "1", False),
-                                   ("@,", "", True),]
+                return [
+                    ("0@", "1", False),
+                    ("1@", "2", False),
+                    ("2@", "3", False),
+                    ("3@", "4", False),
+                    ("4@", "5", False),
+                    ("5@", "6", False),
+                    ("6@", "7", False),
+                    ("7@", "8", False),
+                    ("8@", "9", False),
+                    ("9@", "@0", False),
+                    ("@@", "1", False),
+                    ("@", "", True)
+                ]
+            case 2:
+                return [
+                    ("9@", "8", False),
+                    ("8@", "7", False),
+                    ("7@", "6", False),
+                    ("6@", "5", False),
+                    ("5@", "4", False),
+                    ("4@", "3", False),
+                    ("3@", "2", False),
+                    ("2@", "1", False),
+                    ("1@", "0", False),
+                    ("0@", "@9", False),
+                    ("@0", "", False),
+                    ("@", "", True),
+                ]
             case _:
-                raise Exception("Говно вопрос")
+                raise Exception()
                 
